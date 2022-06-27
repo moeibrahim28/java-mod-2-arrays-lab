1955f5fb2c07d170766b2fcc30f0c9c9dc3911f6
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-27 10:59:00 -0400

    Loop to takes names then print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[10];
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Enter student's name:");
+            names[i] = scanner.nextLine();
+        }
+
+        System.out.println("You hasve entered the following names:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        scanner.close();
     }
 }
